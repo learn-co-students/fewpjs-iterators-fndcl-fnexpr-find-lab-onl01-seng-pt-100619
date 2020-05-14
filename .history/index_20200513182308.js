e53f197db71e2796080595fa16923bf6e27eb19b
steelerx155 <steelerx155@gmail.com>
2020-05-13 21:22:32 -0400

    Done.

---
+++ .history/index_20200513182308.js
@@ -0,0 +1,12 @@
+const testVar = {}
+
+function testFunc() {
+  return "hi"
+}
+
+function superBowlWin(year, result){
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+
+}
