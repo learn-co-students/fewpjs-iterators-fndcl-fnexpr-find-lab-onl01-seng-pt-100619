5b83b7d45b187e5c347c422b1934714a52777faf
Mel <melchrstmn@gmail.com>
2020-05-19 19:01:12 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const winner =  record.find(r => {
+    return r.result === "W";
+  });
+  if (winner) {
+    return winner.year;
+  } else {
+    return undefined
+  }
+};
