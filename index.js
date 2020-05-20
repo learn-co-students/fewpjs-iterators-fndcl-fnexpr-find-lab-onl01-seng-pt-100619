c0b11facb332d1651f790114f83d4fd4c34a8d52
jayson_kesterson <your_email@example.com>
2020-05-19 20:33:46 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winYear = array.find(game => game.result === "W")
+  return winYear ? winYear.year : undefined
+}
