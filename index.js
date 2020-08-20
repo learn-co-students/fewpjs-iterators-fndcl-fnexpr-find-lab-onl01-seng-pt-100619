7809a484c40a32afe9b879d95146aae3aff2a667
Jmiller1294 <jayman1294@gmail.com>
2020-08-20 07:00:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
-let superbowlWin = (array) => {
-  let result = array.find(s => 
-    s.result === 'W')
-  if (result) {
-    return result.year
-  }
-  else  {
-    return  undefined
-  }
+function superbowlWin(record) {
+   let rec = record.find(element => element.result === "W")
+    if (rec) {
+      return rec.year
+    }
+    else {
+      return undefined
+    }
 }
